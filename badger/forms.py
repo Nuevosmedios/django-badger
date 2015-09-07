761bed38d8b76423dfc907b58dd64cdde3f0edfc
henry8812 <henry.gomez@nuevosmedios.ws>
2015-09-07 17:30:17 -0500

    Update forms.py

--- badger/forms.py
+++ badger/forms.py
@@ -158,7 +158,7 @@ class BadgeEditForm(MyModelForm):
 
     class Meta:
         model = Badge
-        fields = ('title', 'image', 'description','prerequisites', 'group_badges')
+        fields = ('title', 'image', 'description','prerequisites', 'group_badge')
         try:
             # HACK: Add "tags" as a field only if the taggit app is available.
             import taggit
